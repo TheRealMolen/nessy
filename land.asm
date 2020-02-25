09875b50f79b8b31f4a6628ddf4aed60ad54803b
Alex Mole <TheRealMolen@users.noreply.github.com>
2020-02-25 23:36:36 +0000

    ground

--- land.asm
+++ land.asm
@@ -13,7 +13,8 @@ land:
 	.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
 	.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
 	.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
-	.byte $ec,$ed,$ee,$ef,$ec,$ed,$ec,$ed,$ee,$ef,$ee,$ef,$ec,$ee,$ef,$ed
+land_ground:	
+        .byte $ec,$ed,$ee,$ef,$ec,$ed,$ec,$ed,$ee,$ef,$ee,$ef,$ec,$ee,$ef,$ed
 	.byte $ec,$ed,$ee,$ef,$ed,$ee,$ec,$ed,$ee,$ef,$ee,$ef,$ed,$ed,$ee,$ef
 	.byte $fc,$fd,$fe,$ff,$fc,$fd,$fc,$fd,$fe,$ff,$fe,$ff,$fc,$fe,$ff,$fd
 	.byte $fc,$fd,$fe,$ff,$fd,$fe,$fc,$fd,$fe,$ff,$fe,$ff,$fd,$fd,$fe,$ff
