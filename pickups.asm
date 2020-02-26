32a1045e30e6f85ae8fbcd9e18837b029a475908
Alex Mole <TheRealMolen@users.noreply.github.com>
2020-02-26 18:37:34 +0000

    work on pickup speed

--- pickups.asm
+++ pickups.asm
@@ -13,5 +13,16 @@ pu_pos_32:
 	.hex 42
 	.hex 49
 	.hex 69
-	
+        
+;; sam's max speed when carrying N pickups
+; NB this is x4
+pu_maxspeed:
+	.byte 12
+	.byte 11
+	.byte 10
+	.byte 19
+	.byte 8
+	.byte 7
+	.byte 6
+	.byte 4
 
