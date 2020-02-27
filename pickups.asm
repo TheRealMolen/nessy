5276df1ad3fb9e281d2a1e501b8f4597b956e761
Alex Mole <TheRealMolen@users.noreply.github.com>
2020-02-27 10:33:56 +0000

    pickup pickups

--- pickups.asm
+++ pickups.asm
@@ -3,11 +3,12 @@ num_pickups = 8
 pickup_static_tile = $90
 pickup_carry_tile = $a0
 
+
 ;; pickup positions, div32
 pu_pos_32:
-	.hex 04
-	.hex 05
-	.hex 0a
+	.hex 06
+	.hex 09
+	.hex 11
 	.hex 1b
 	.hex 24
 	.hex 42
@@ -17,12 +18,12 @@ pu_pos_32:
 ;; sam's max speed when carrying N pickups
 ; NB this is x4
 pu_maxspeed:
-	.byte 16
-	.byte 14
+	.byte 14	; 0 pickups
 	.byte 12
 	.byte 10
+	.byte 9
 	.byte 8
-	.byte 7
-	.byte 6
-	.byte 4
+	.byte 5
+	.byte 3
+	.byte 1		; 8 pickups
 
