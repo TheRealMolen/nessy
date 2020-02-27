ff97f5ca3b08324d2fc768c1afde9ef7ddb94a4d
Alex Mole <TheRealMolen@users.noreply.github.com>
2020-02-27 12:37:56 +0000

    some bt rendering

---
+++ pickupdefs.asm
@@ -0,0 +1,29 @@
+
+num_pickups = 8
+pickup_static_tile = $90
+pickup_carry_tile = $a0
+
+
+;; pickup positions, div32
+pu_pos_32:
+	.hex 06
+	.hex 09
+	.hex 11
+	.hex 1b
+	.hex 24
+	.hex 42
+	.hex 49
+	.hex 69
+        
+;; sam's max speed when carrying N pickups
+; NB this is x4
+pu_maxspeed:
+	.byte 14	; 0 pickups
+	.byte 12
+	.byte 10
+	.byte 9
+	.byte 8
+	.byte 5
+	.byte 3
+	.byte 1		; 8 pickups
+
